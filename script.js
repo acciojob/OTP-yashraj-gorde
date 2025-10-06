204cfa77b7e799bec870608413e885c1e26a0cc7
Yashraaj Sharad Gorde <149304566+yashraj-gorde@users.noreply.github.com>
2025-10-06 10:14:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(."code");
+
+codes[0].focus();
+
+codes.forEach((input, index) => {
+	input.addEventListener("input", (e) => {
+		const value = e.target.value;
+		if(value.length === 1 && index < codes.length-1) {
+			codes[index + 1].focus();
+		}
+	});
+	input.addEventListener("keydown", (e) => {
+		if(e.key === "Backspace") {
+			if(input.value === "" && index > 0){
+				codes[index - 1].focus();
+				codes[index - 1].value = "";
+			} else {
+				input.value = "";
+			}
+		}
+	});
+});
